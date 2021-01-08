9571abc1243cda3eb82a673684ceba7620d1aab1
JaymanW <jaymanw2000@gmail.com>
2021-01-07 22:28:44 -0600

    js v2

--- script.js
+++ script.js
@@ -2,9 +2,51 @@ const generateBtn = document.querySelector("#generate");
 
 const lowerCaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperCaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numericOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialOptions = [" ", "!", "\"", "#", "$", "%", "&", "@", "(", ")"];
 
 let passwordLength = 0;
 let passwordType = "";
+let password = [];
+let finalPassword = "";
+
+const assemblePassword = () => {
+  if (passwordType === "lowercase") {
+    for (let i = 0; i < passwordLength; i++) {
+      const randomInt = Math.floor(Math.random() * 25);
+      password.push(lowerCaseOptions[randomInt]);
+    }
+    finalPassword = password.join("");
+    console.log(finalPassword);
+  }
+
+  if (passwordType === "uppercase") {
+    for (let i = 0; i < passwordLength; i++) {
+      const randomInt = Math.floor(Math.random() * 25);
+      password.push(upperCaseOptions[randomInt]);
+    }
+    finalPassword = password.join("");
+    console.log(finalPassword);
+  }
+
+  if (passwordType === "numeric") {
+    for (let i = 0; i < passwordLength; i++) {
+      const randomInt = Math.floor(Math.random() * 9);
+      password.push(numericOptions[randomInt]);
+    }
+    finalPassword = password.join("");
+    console.log(finalPassword);
+  }
+
+  if (passwordType === "special") {
+    for (let i = 0; i < passwordLength; i++) {
+      const randomInt = Math.floor(Math.random() * 9);
+      password.push(specialOptions[randomInt]);
+    }
+    finalPassword = password.join("");
+    console.log(finalPassword);
+  }
+}
 
 
 const generatePassword = () => {
@@ -34,6 +76,7 @@ const generatePassword = () => {
         alert("Please input one of the listed TYPE options")
       }
     }
+    assemblePassword();
 }
 
 // const determineLength = () => {
